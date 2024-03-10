9bf6be6a8c846a8e3591e9c5b22d69953a6ee6ed
conghung43 <conghung43@gmail.com>
2024-03-10 18:22:21 +0800

    update main.py

--- Main.py
+++ Main.py
@@ -16,10 +16,12 @@ import numpy as np
 import cv2
 import json
 from collections import Counter
+import base64
 
 class ClickableLabel(QLabel):
     def __init__(self, text, parent=None):
         super().__init__(text, parent)
+        self.setVisible(True)
         self.mousePressEvent = self.on_click
 
     def on_click(self, event):
@@ -47,16 +49,15 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.classes = ['cell','glass_cell','plastic','copper']
+        self.classes = ["cell","glass_cell","plastic","copper"]
         self.Group_image_index = [0]*len(self.classes)
         self.annotationIndex = -1
         self.group_boxes = []  # List to store the created group boxes
-        # self.images_path = "/Users/hungnguyencong/Downloads/hung.PNG"
-        # image = cv2.imread(self.images_path)
-        # self.images_path_lbl.setText("Desktop/book_images")
-        # self.db_directory = '/Users/hungnguyencong/Desktop/DU AN CUA HUNG/PYTHON_CODE/django_flashcard'
+
         self.LoadFolderBtn.clicked.connect(self.open_input_folder_dialog)
         self.CreateGroupBox(len(self.classes))
+
+        self.open_input_folder_dialog()
 
         # for i in range(numberOfImage):
         #     self.SetImageToGroup(image, 1, i,self.numberOfColumn)
@@ -79,18 +80,18 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         heightWidthGroupBoxRatio = self.size().height()//class_size
         self.GroupBoxArea = heightWidthGroupBoxRatio*class_size*class_size
         for idx in range(numberOfClass):
-            group_box = QGroupBox(self)#(f'Group {idx+1}')  # Create QGroupBox with a title
+            group_box = QGroupBox(self)#(f"Group {idx+1}")  # Create QGroupBox with a title
             group_box.setGeometry(QRect(buffer +idx*(class_size + 2*buffer), 50, int(class_size), self.size().height()))
             self.group_boxes.append(group_box)  # Append the created group box to the list
 
     def SetImageToGroup(self, image, group_index, image_index, numberOfColumn, element_dict):
         row = image_index//numberOfColumn
         column = image_index%numberOfColumn
-        element_dict['position'] = f'{group_index}_{row}_{column}'
-        imageLabel = ClickableLabel(element_dict['position'],self.group_boxes[group_index])
+        element_dict["position"] = f"{group_index}_{row}_{column}"
+        imageLabel = ClickableLabel(element_dict["position"],self.group_boxes[group_index])
         imageLabel.setGeometry(QRect((image_index%numberOfColumn)*self.imageLabelEdgeSize, (image_index//numberOfColumn)*self.imageLabelEdgeSize, self.imageLabelEdgeSize, self.imageLabelEdgeSize))
         imageLabel.setStyleSheet("background-color: rgb(251, 255, 230);")
-        imageLabel.setObjectName(element_dict['position'])
+        imageLabel.setObjectName(element_dict["position"])
         image = self.ConvertToQpixmap(image,imageLabel)
         imageLabel.setPixmap(image)
         imageLabel.show()
@@ -121,21 +122,24 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
                         labels.append(child.objectName())
                     child.deleteLater()
             #Delete
-            for item in self.currentAnnotation['shapes'][:]: 
-                if item['position'] in labels:
-                    self.currentAnnotation['shapes'].remove(item)
+            if '.txt' in self.annotation_files_path[self.annotationIndex]:
+                labels = []
+            for item in self.currentAnnotation["shapes"][:]: 
+                if item["position"] in labels:
+                    self.currentAnnotation["shapes"].remove(item)
                 else:
-                    del item['position']
+                    del item["position"]
         except:
-            print('find_labels_in_groupbox_and_delete')
+            print("find_labels_in_groupbox_and_delete")
             return False
         return True
     
     def SaveAnnotationToJson(self):
-        #Create backup_file
+        if ".txt" in self.annotation_files_path[self.annotationIndex]:
+            self.annotation_files_path[self.annotationIndex]= self.annotation_files_path[self.annotationIndex].replace(".txt",".json")
 
         #Save/overwrite file
-        with open(self.annotation_files_path[self.annotationIndex], 'w') as json_file:
+        with open(self.annotation_files_path[self.annotationIndex], "w") as json_file:
             json.dump(self.currentAnnotation, json_file)
 
     def UploadImagesToUI(self):
@@ -144,34 +148,37 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         # for i in range(20):
         #     self.SetImageToGroup(image, 1, i,self.numberOfColumn)
         # return
-        for inx in range(len(self.currentAnnotation['shapes'])):
+        for inx in range(len(self.currentAnnotation["shapes"])):
             label,bbox, element_dict = self.GetImageAnnotation(inx)
             group_index = self.classes.index(label)
             crop_image = self.currentImage[bbox[1]:bbox[3],bbox[0]:bbox[2]]
-            self.SetImageToGroup(crop_image,group_index,self.Group_image_index[group_index],self.numberOfColumn, element_dict)
+            if 0 not in crop_image.shape:
+                self.SetImageToGroup(crop_image,group_index,self.Group_image_index[group_index],self.numberOfColumn, element_dict)
             self.Group_image_index[group_index] += 1
             # break
             
     def GetImageAnnotation(self, inx):
-        element_dict = self.currentAnnotation['shapes'][inx]
-        label = element_dict['label']
-        bbox = element_dict['description'].split(',')
+        element_dict = self.currentAnnotation["shapes"][inx]
+        label = element_dict["label"]
+        bbox = element_dict["description"].split(",")
         bbox = [int(x) for x in bbox]
         return label,bbox,element_dict
 
 
     def ReadAnnotationInit(self):
         files = os.listdir(self.annotation_dir_path)
-        image_type = '.PNG'
-        annotationType= '.json'
+        self.image_type = ".PNG"
+        self.annotationType= ".json"
         self.annotationIndex = 0
-        self.annotation_files_path = [os.path.join(self.annotation_dir_path, file) for file in files if file.endswith(annotationType)]
+        self.annotation_files_path = [os.path.join(self.annotation_dir_path, file) for file in files if file.endswith(self.annotationType)]
 
         if len(self.annotation_files_path) == 0:
-            image_type = '.jpg'
-            annotationType = '.txt'
-            self.annotation_files_path = [os.path.join(self.annotation_dir_path, file) for file in files if file.endswith(annotationType)]
-        self.currentImage = self.ReadImage(self.annotation_files_path[self.annotationIndex].replace(annotationType,image_type))
+            self.image_type = ".jpg"
+            self.annotationType = ".txt"
+            self.annotation_files_path = [os.path.join(self.annotation_dir_path, file) for file in files if file.endswith(self.annotationType)]
+        self.currentImage = self.ReadImage(self.annotation_files_path[self.annotationIndex].replace(self.annotationType,self.image_type))
+        if self.currentImage is None:
+            self.currentImage = self.ReadImage(self.annotation_files_path[self.annotationIndex].replace(self.annotationType,".jpg"))
         self.currentAnnotation = self.ReadJson(self.annotation_files_path[self.annotationIndex],self.currentImage)
         self.FileNameTxt.setText(os.path.basename(self.annotation_files_path[self.annotationIndex]))
     
@@ -184,36 +191,54 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     def ReadJson(self, file_path,image):
         # Open the JSON file
         data = {}
-        if 'json' in file_path:
-            with open(file_path, 'r') as file:
+        if "hot-20240205-152751-022-3151275534876_0_0" in file_path:
+            print()
+        if ".json" in file_path:
+            with open(file_path, "r") as file:
                 # Load the JSON data
                 data = json.load(file)
-                for inx in range(len(data['shapes'])):
-                    polypoints = np.array(data['shapes'][inx]['points'])
-                    topLeft = (np.min(polypoints[:,0]), np.min(polypoints[:,1]))
-                    rightBottom = (np.max(polypoints[:,0]), np.max(polypoints[:,1]))
-                    data['shapes'][inx]['description'] = f'{int(topLeft[0])},{int(topLeft[1])},{int(rightBottom[0])},{int(rightBottom[1])}'
+                for inx in range(len(data["shapes"])):
+                    polypoints = np.array(data["shapes"][inx]["points"])
+                    try:
+                        topLeft = (np.min(polypoints[:,0]), np.min(polypoints[:,1]))
+                        rightBottom = (np.max(polypoints[:,0]), np.max(polypoints[:,1]))
+                        data["shapes"][inx]["description"] = f"{int(topLeft[0])},{int(topLeft[1])},{int(rightBottom[0])},{int(rightBottom[1])}"
+                    except:
+                        print()
         # Read TXT
         else:
-            data['shapes'] = []
+            data["shapes"] = []
+            data["flags"] = {}
+            data["imagePath"] = file_path.replace(".txt", ".jpg")#"imageHeight": 3672, "imageWidth": 5488
+            data["imageHeight"] = image.shape[0]
+            data["imageWidth"] = image.shape[1]
+            data["version"] = "5.3.0"
+            _, img_encoded = cv2.imencode(".jpg", image)
+            data["imageData"] = base64.b64encode(img_encoded).decode("utf-8")
             with open(file_path, "r") as file:
                 # Read all lines from the file into a list
                 lines = file.readlines()
                 for line in lines:
-                    lineSplit = line.replace('\n','').split(' ')
+                    lineSplit = line.replace("\n","").split(" ")
                     arr = np.array(lineSplit[1:], dtype = float).reshape(-1, 2)*np.array([image.shape[1],image.shape[0]])
+                    if len(arr) == 0: continue
                     topLeft = (np.min(arr[:,0]), np.min(arr[:,1]))
                     rightBottom = (np.max(arr[:,0]), np.max(arr[:,1]))
-                    data['shapes'].append({'label': self.classes[int(lineSplit[0])],'description':f'{int(topLeft[0])},{int(topLeft[1])},{int(rightBottom[0])},{int(rightBottom[1])}'})
+                    data["shapes"].append({"label": self.classes[int(lineSplit[0])],
+                                           "description":f"{int(topLeft[0])},{int(topLeft[1])},{int(rightBottom[0])},{int(rightBottom[1])}",
+                                           "points":arr.tolist(),
+                                           "group_id":None,
+                                           "shape_type": "polygon", "flags": {}
+                                           })
         
         
-        numberOfImage = max(dict(Counter(shape['label'] for shape in data['shapes'])).values())
+        numberOfImage = max(dict(Counter(shape["label"] for shape in data["shapes"])).values())
         self.imageLabelEdgeSize = int(np.sqrt(self.GroupBoxArea/numberOfImage))
         self.numberOfColumn = int(self.group_boxes[0].size().width()/self.imageLabelEdgeSize)
-
+        if self.numberOfColumn ==0: self.numberOfColumn = 1
         return data
 
-        # Now you can work with the 'data' object, which contains the contents of the JSON file
+        # Now you can work with the "data" object, which contains the contents of the JSON file
 
 
     def fix_size(self,fn, desired_w=256, desired_h=256, fill_color=(0, 0, 0, 255)):
@@ -230,7 +255,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             h = y
             w = int(h * desired_ratio)
 
-        new_im = Image.new('RGBA', (w, h), fill_color)
+        new_im = Image.new("RGBA", (w, h), fill_color)
         new_im.paste(im, ((w - x) // 2, (h - y) // 2))
         return new_im.resize((desired_w, desired_h))
 
@@ -238,7 +263,8 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     #     self.ClassNameGroup_1.setGeometry(QRect(self.class_pannel_size, 50, 121, 391))
 
     def open_input_folder_dialog(self):
-        self.annotation_dir_path = QFileDialog.getExistingDirectory(self,"Select input annotations folder", "",QFileDialog.ShowDirsOnly)
+        # self.annotation_dir_path = QFileDialog.getExistingDirectory(self,"Select input annotations folder", "",QFileDialog.ShowDirsOnly)
+        self.annotation_dir_path = r"E:/yolov9/data/0218/outputSegV8 - Copy/YoloV8Output"
         if not self.annotation_dir_path:
             return
         else:
@@ -246,8 +272,10 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
                 self.directoryPathTxt.setText(self.annotation_dir_path)
                 self.ReadAnnotationInit()
                 self.UploadImagesToUI()
-        # self.db_directory = '/Users/hungnguyencong/Desktop/DU AN CUA HUNG/PYTHON_CODE/django_flashcard'
-        # self.reload_ui_display_after_picking_db()
+
+                for i in range(len(self.annotation_files_path)):
+                    self.next_and_save()
+
 
     def removeChildren(self):
         for group_box in self.group_boxes:
@@ -262,16 +290,16 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     def read_display_txt(self):
         try:
             if self.index == 1:
-                self.index = int(open(os.path.join(self.annotation_dir_path,f'page.txt'), "r").read())
+                self.index = int(open(os.path.join(self.annotation_dir_path,f"page.txt"), "r").read())
         except:
             pass
         try:
             self.page_number.setText(f"{self.index}")
-            f = open(os.path.join(self.annotation_dir_path,f'{self.index}.txt'), "r")
+            f = open(os.path.join(self.annotation_dir_path,f"{self.index}.txt"), "r")
             self.textEdit_page_content.setPlainText(f.read())
         except Exception as ex:
             print(ex)
-            txt_paths = glob.glob(os.path.join(self.annotation_dir_path,'*.*'))
+            txt_paths = glob.glob(os.path.join(self.annotation_dir_path,"*.*"))
             if self.index > len(txt_paths):
                 self.page_number.setText(f"{self.index}/{len(txt_paths)} Done!")
 
@@ -279,15 +307,16 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         
         self.find_labels_in_groupbox_and_delete()
         self.SaveAnnotationToJson()
-        #self.removeChildren()
-
+        print(self.annotation_files_path[self.annotationIndex])
         #Load new annotation
         self.annotationIndex += 1
         if len(self.annotation_files_path) <= self.annotationIndex:
             return
         self.FileNameTxt.setText(os.path.basename(self.annotation_files_path[self.annotationIndex]))
-        self.currentAnnotation = self.ReadJson(self.annotation_files_path[self.annotationIndex])
-        self.currentImage = self.ReadImage(self.annotation_files_path[self.annotationIndex].replace('json','PNG'))
+        self.currentImage = self.ReadImage(self.annotation_files_path[self.annotationIndex].replace(self.annotationType,self.image_type))
+        if self.currentImage is None:
+            self.currentImage = self.ReadImage(self.annotation_files_path[self.annotationIndex].replace(self.annotationType,".jpg"))
+        self.currentAnnotation = self.ReadJson(self.annotation_files_path[self.annotationIndex],self.currentImage)
         
         self.Group_image_index = [0]*len(self.classes)
         self.UploadImagesToUI()
--- YoloV8Inference.py
+++ YoloV8Inference.py
@@ -5,9 +5,75 @@
 # ultralytics.checks()
 
 from ultralytics import YOLO
+import os
+#from IPython.display import display, Image
+import numpy as np
+from PIL import Image
+import shutil
 
-from IPython.display import display, Image
 
-model = YOLO(r'Model\best.pt')
-results = model.predict(source=r"E:\yolov9\data\0218\outputSegV8\valid\images\CV3-2000M-19RT-Snapshot-20240205-152926-387-3152229228420_0_2.jpg", conf=0.25)
+def get_image_paths(directory):
+    image_paths = []
+    # Traverse through the directory and its subdirectories
+    directories = [os.path.join(directory, d) for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
+    for mainFolder in directories:
+        subFolders = directories = [os.path.join(mainFolder, d) for d in os.listdir(mainFolder) if os.path.isdir(os.path.join(mainFolder, d))]
+        for folder in subFolders:
+            image_path = GetImageFromFolder(folder)
+            if len(image_path) == 0:
+                continue
+            image_paths.extend(image_path)
+    return image_paths
+
+def GetImageFromFolder(directory):
+    files = os.listdir(directory)
+    images_files_path = [os.path.join(directory, file) for file in files if file.endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+    return images_files_path
+
+
+
+#imagesPath = get_image_paths(r"E:\yolov9\data\0218\outputSegV8")
+
+directory = r"E:\yolov9\data\0218\outputSegV8 - Copy"
+exportFolderName = 'YoloV8Output'
+exportFolderPath = os.path.join(directory,exportFolderName)
+if not os.path.exists(exportFolderPath):
+    os.makedirs(exportFolderPath)
+image_paths = get_image_paths(directory)
+model = YOLO(r'C:\Users\kai\Downloads\best.pt')
+
+for imagePath in image_paths:
+    print(imagePath)
+    # write image and annotation
+    newTxtName = os.path.basename( imagePath.replace('.jpg','.txt'))
+    newImageName = os.path.basename(imagePath)
+
+    if os.path.exists( newImageName):
+        continue
+    results = model.predict(source=imagePath, conf=0.25, save=True)
+
+    if results[0].masks is None:
+        continue
+
+    masks = results[0].masks.xy
+    cls = results[0].boxes.cls.numpy()
+    #image = Image.open(imagePath)
+    subWidth,subHeight = results[0].orig_shape
+
+    newTxtPath = os.path.join(exportFolderPath,newTxtName)
+    newImagePath = os.path.join(exportFolderPath,newImageName)
+
+    with open(newTxtPath, 'w') as file:
+        for idx, ann in enumerate( masks):
+            ann = ann/np.array([subWidth,subHeight])
+            contentPrepared = f'{int(cls[idx])}'
+            for index in range(len(ann)):
+                contentPrepared += f' {ann[index][0]} {ann[index][1]}'
+                if index == len(ann) -1:
+                    contentPrepared += '\n'
+            file.write(contentPrepared)
+
+                #draw.rectangle(ann[1:], outline="red")
+    shutil.copy(imagePath, newImagePath)
+
 print(results[0].masks.xy)
